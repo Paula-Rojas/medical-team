a5844989213e9cdebb535fac2f35509544867ee3
Paula-Rojas <150094276+Paula-Rojas@users.noreply.github.com>
2023-11-09 11:24:01 -0500

    infografia

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function showImage(imageId) {
   const images = document.querySelectorAll('.image');
   images.forEach(image => image.classList.remove('active'));
-  const selectedImage = document.getElementById(imageId);
-  selectedImage.classList.add('active');
+  const selectedImages = document.getElementById(imageId);
+  selectedImages.forEach(selectedImage => selectedImage.classList.add('active'));
 }
